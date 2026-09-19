3b7ba734dafcd2eb570e249f0d5188fc413d4830
rohity0611 <rohityadav6643@gmail.com>
2026-09-19 12:45:24 +0530

    feat: add RY favicon with brand styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,11 @@ export const metadata: Metadata = {
     "West Bengal",
   ],
   authors: [{ name: "Rohit Yadav" }],
+  icons: {
+    icon: "/favicon.svg",
+    shortcut: "/favicon.svg",
+    apple: "/favicon.svg",
+  },
   openGraph: {
     type: "website",
     locale: "en_US",
